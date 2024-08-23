8464d93e91926ec49f9cba00746a3ee1aa7e08e6
Arthur Carvalho <artprozew@gmail.com>
2024-08-22 23:30:48 -0300

    refactor: add ApplicationDbContext to Infra.Data

---
+++ CarRental.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CarRental.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRental.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Car> Car { get; set; }
+        public DbSet<Rental> Rental { get; set; }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}

dc042dc73066a419a86c06ae812f1081beb59f42
Arthur Carvalho <artprozew@gmail.com>
2024-09-03 23:19:25 -0300

    refactor: add IUserRepository

---
+++ CarRental.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using CarRental.Domain.Entities;
+
+namespace CarRental.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateAsync(User user);
+        Task<User> UpdateAsync(User user);
+        Task<User?> DeleteAsync(int id);
+        Task<User?> GetAsync(int id);
+        Task<IEnumerable<User>> GetAllAsync();
+    }
+}
